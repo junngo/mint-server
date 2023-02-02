c0fab1904e25da4d4514ada708fadf9daffb9bb9
junngo <myeongjun.ko@gmail.com>
2023-02-02 16:57:41 +0900

    creating the job to get the stock data

--- stock/models.py
+++ stock/models.py
@@ -8,6 +8,7 @@ class Stock(models.Model):
 
     ticker = models.CharField(max_length=20)
     ticker_name = models.CharField(max_length=20)
+    country = models.CharField(max_length=20)
 
 
 class StockPrice(models.Model):
@@ -15,12 +16,12 @@ class StockPrice(models.Model):
     Stock Price Model
     """
 
-    stock_id = models.ForeignKey(Stock, on_delete=models.SET_NULL)
+    stock_id = models.ForeignKey(Stock, on_delete=models.PROTECT)
     stock_date = models.DateField()
-    open_price = models.DecimalField(max_digits=8, decimal_places=6, default=None, blank=True, null=True)
-    high_price = models.DecimalField(max_digits=8, decimal_places=6, default=None, blank=True, null=True)
-    low_price = models.DecimalField(max_digits=8, decimal_places=6, default=None, blank=True, null=True)
-    close_price = models.DecimalField(max_digits=8, decimal_places=6, default=None, blank=True, null=True)
-    adj_close_price = models.DecimalField(max_digits=8, decimal_places=6, default=None, blank=True, null=True)
+    open_price = models.DecimalField(max_digits=14, decimal_places=6, default=None, blank=True, null=True)
+    high_price = models.DecimalField(max_digits=14, decimal_places=6, default=None, blank=True, null=True)
+    low_price = models.DecimalField(max_digits=14, decimal_places=6, default=None, blank=True, null=True)
+    close_price = models.DecimalField(max_digits=14, decimal_places=6, default=None, blank=True, null=True)
+    adj_close_price = models.DecimalField(max_digits=14, decimal_places=6, default=None, blank=True, null=True)
     volume = models.IntegerField(default=None, blank=True, null=True)
-    change = models.DecimalField(max_digits=8, decimal_places=6, default=None, blank=True, null=True)
+    change = models.DecimalField(max_digits=14, decimal_places=6, default=None, blank=True, null=True)
--- stock/tests.py
+++ stock/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from stock.models import Stock, StockPrice
+from stock.views import getStockPrice
 
 # Create your tests here.
+class StockTestCase(TestCase):
+
+    def setUp(self) -> None:
+        Stock.objects.create(ticker="005930", ticker_name="삼성전자")
+        # Stock.objects.create(ticker="066570", ticker_name="lg전자")
+        Stock.objects.create(ticker="SPY", ticker_name="SPY")
+        # Stock.objects.create(ticker="EFA", ticker_name="EFA")
+
+
+    def test_my_test(self):
+        getStockPrice()
+        # stocks = Stock.objects.all()
+        # self.assertEqual(1, stocks.count())
