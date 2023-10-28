6236187420ccd5e13d8fc324b2b3e5dbc39bd5b3
junngo <myeongjun.ko@gmail.com>
2023-10-28 17:44:59 +0900

    changed setting value to get it from the .env

--- config/settings.py
+++ config/settings.py
@@ -21,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kta2(vvyf9l!v12&(1nei2a+^qr4(%=f3b0=s%%3wd&-1iau)$'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-kta2(vvyf9l!v12&(1nei2a+^qr4(%=f3b0=s%%3wd&-1iau)$')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get('DEBUG', True))
 
-ALLOWED_HOSTS = []
+if os.environ.get('ALLOWED_HOSTS'):
+    ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(' ')
+else:
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
