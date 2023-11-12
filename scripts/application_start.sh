506d68329c78f6980260c1c1206c03bfc4d7398a
junngo <myeongjun.ko@gmail.com>
2023-11-12 12:46:53 +0900

    modified code deploy script

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,3 +1,5 @@
+cp /home/ec2-user/work/stock_config.yaml /home/ec2-user/work/mint-server/stock_config.yaml
 cd /home/ec2-user/work/mint-server
 
+docker-compose -f docker-compose.prod.yml build
 docker-compose -f docker-compose.prod.yml up -d
